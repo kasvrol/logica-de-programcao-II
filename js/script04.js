4a1c86eec69e97385ccb5e267a81724d606dc0c6
ana.karolina <ana.karolina@ablsystem.com.br>
2022-05-30 16:26:06 -0300

    lógica para criação a cara do Creeper

---
+++ js/script04.js
@@ -0,0 +1,21 @@
+const tela = document.querySelector('canvas');
+const pincel = tela.getContext('2d');
+
+pincel.fillStyle = 'green';
+pincel.fillRect(0, 0, 350, 300)
+
+pincel.fillStyle = 'black';
+pincel.fillRect(50, 30, 90, 90)
+
+pincel.fillStyle = 'black';
+pincel.fillRect(210, 30, 90, 90)
+
+pincel.fillStyle = 'black';
+pincel.fillRect(140, 120, 70, 100)
+
+pincel.fillStyle = 'black';
+pincel.fillRect(100, 190, 40, 110)
+
+pincel.fillStyle = 'black';
+pincel.fillRect(210, 190, 40, 110)
+
