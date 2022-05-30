66e265e45f0f747cd0836176a91725f48a0af993
ana.karolina <ana.karolina@ablsystem.com.br>
2022-05-30 16:34:42 -0300

    feat: lógica para criação de dois esquadros

---
+++ js/script05.js
@@ -0,0 +1,36 @@
+const tela = document.querySelector('canvas');
+const pincel = tela.getContext('2d');
+
+function esquadroMaior() {
+    pincel.fillStyle = 'black';
+
+    pincel.beginPath();
+
+    pincel.moveTo(50, 50);
+
+    pincel.lineTo(50, 400);
+
+    pincel.lineTo(400, 400);
+
+    pincel.fill()
+}
+
+function esquadroMenor() {
+    pincel.fillStyle = 'white';
+
+    pincel.beginPath();
+
+    pincel.moveTo(100, 175);
+
+    pincel.lineTo(100, 350);
+
+    pincel.lineTo(275, 350);
+
+    pincel.fill()
+}
+
+esquadroMaior()
+esquadroMenor()
+
+
+
